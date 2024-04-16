66adbc319d56947119605fba5fad505819d3683c
anwe712 <ghoshanwesha272@gmail.com>
2024-04-16 20:04:31 +0530

    #include - why used

---
+++ Basic InputandOutput/ProgramToDemonstrate#include_directive.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Include the standard input-output library header file
+
+//The #include directive in C (and C++) is used to include the contents of another file (usually a header file) into your source code file. 
+
+int main() {
+    // Print a message to the console
+    printf("Hello, world!\n");
+
+    return 0;
+}
