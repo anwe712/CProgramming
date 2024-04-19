924fec4d7d29f3fd5570b19af5059bcf52485570
anwe712 <ghoshanwesha272@gmail.com>
2024-04-19 12:50:09 +0530

    code added

---
+++ Enumeration/colours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Enum declaration
+enum Color {
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    CYAN,
+    MAGENTA
+};
+
+int main() {
+    // Print the names of each color
+    printf("Colors:\n");
+    printf("RED\n");
+    printf("GREEN\n");
+    printf("BLUE\n");
+    printf("YELLOW\n");
+    printf("CYAN\n");
+    printf("MAGENTA\n");
+
+    return 0;
+}
